47da29874362a6a7f8953c8b8bab53e3e6139fcc
Jacques Wiese <Jacques.a.wiese@gmail.com>
2016-08-18 22:51:46 -0500

    First Commit

---
+++ TeamPowered/TeamPowered/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamPowered
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
